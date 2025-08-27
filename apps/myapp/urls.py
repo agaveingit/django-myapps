de4da14cde377413bc48a14c5162cbf61448c3e5
agevavava <holhokgorgok@gmail.com>
2025-08-27 09:36:27 +0700

    nambah fitur konversi

--- apps/myapp/urls.py
+++ apps/myapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("konversi/", views.konversi, name="konversi")
 ]
--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render, HttpResponse
+from .konversi import teks_dari_angka
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
     # return HttpResponse("hi")
+
+def konversi(request):
+    result = None
+    error = None
+
+    if request.method == "POST":
+        angka = request.POST.get("angka")
+        try:
+            angka = int(angka)
+            result = teks_dari_angka(angka)
+        except (ValueError, TypeError):
+            error = "Error! Harus masukan angka."
+
+    return render(request, "konversi.html", {"result": result, "error": error})
