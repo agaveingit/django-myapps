21b4b666839bfa42c1f859be449016b47c4f0731
agevavava <holhokgorgok@gmail.com>
2025-08-31 08:21:14 +0700

    now its up to trillion

--- apps/myapp/alat/konversi.py
+++ apps/myapp/alat/konversi.py
@@ -4,12 +4,17 @@ class Konverter:
     sesuai dengan input yang diberikan.
 
     Kalkulasi dilakukan dengan cara mencari bilangan dan posisinya.
-    Batasan pada kode ini adalah limit angkanya sendiri yang hanya sampai 999.
-    Kode ini juga belum bisa melakukan konversi bilangan negatif.
     """
     def __init__(self):
         self.satuan: list[str] = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan']
-        self.belasan: list[str] = ["sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", "enam belas", "tujuh belas", "delapan belas", "sembilan belas"]
+        self.belasan: list[str] = ["sepuluh", "sebelas", "dua belas", "tiga belas", "empat belas", "lima belas", 
+                                    "enam belas", "tujuh belas", "delapan belas", "sembilan belas"]
+        self.angka_level_tinggi: list[tuple[int, str]] = [
+            (1_000_000_000_000, "triliun"),
+            (1_000_000_000, "miliar"),
+            (1_000_000, "juta"),
+            (1_000, "ribu"),
+        ]
 
     def puluhan(self, i: int) -> str:
         puluh: int = int(i / 10)
@@ -27,21 +32,50 @@ class Konverter:
     def ratusan(self, i: int) -> str:
         ratus: int = int(i / 100)
         sisa: int = i % 100
-        if i < 200:
-            if (sisa) == 0:
-                return "seratus"
-            else:
-                return f"seratus {self.puluhan(sisa)}"
+        if i < 100:
+            return self.puluhan(i)
+        elif i < 200:
+            return f"seratus {self.puluhan(sisa)}"
         else:
-            if (sisa) == 0:
-                return f"{self.satuan[ratus]} ratus"  
-            else:
-                return f"{self.satuan[ratus]} ratus {self.puluhan(sisa)}"
+            return f"{self.satuan[ratus]} ratus {self.puluhan(sisa)}"
 
     def konversi(self, angka: int) -> str:
-        if angka < 100:
-            return self.puluhan(angka)
-        elif angka < 1000:
+        if angka < 0:
+            return f"minus {self.konversi(-angka)}"
+        
+        if angka == 0:
+            return "nol"
+        
+        if angka < 1000:
             return self.ratusan(angka)
-        else:
-            return "Error!: Maksimal input adalah 999"
+
+        # Kode dilakukan dengan mencari nilai yang setara dengan
+        # input yang diberikan. Dimulai dari level tertinggi
+        # menuju level yang lebih rendah.
+        for nilai, nama in self.angka_level_tinggi:
+            if angka >= nilai:
+                depan = angka // nilai
+                sisa = angka % nilai
+
+                if nilai == 1_000 and depan == 1:
+                    head = "seribu"
+                else:
+                    head = f"{self.konversi(depan)} {nama}"
+
+                return head if sisa == 0 else f"{head} {self.konversi(sisa)}"
+
+        # fallback 
+        return ""
+
+# if __name__ == "__main__":
+#     """
+#     Mengunakan input()
+#     dapat dilakukan dalam proses ujicoba
+#     """
+#     konversi_angka = Konverter()
+#     try:
+#         contoh_angka: int = 979
+#         hasil: str = konversi_angka.konversi(contoh_angka) # Ekspektasi output = sembilan ratus tujuh puluh sembilan
+#         print(hasil)
+#     except (ValueError, TypeError):
+#         print("Error!: Harus masukan angka")
