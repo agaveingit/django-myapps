17f1212b642914afb647354df1f3986672c2c5c0
agevavava <holhokgorgok@gmail.com>
2025-09-01 18:59:42 +0700

    add some url

--- apps/myapp/urls.py
+++ apps/myapp/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("konversi/", views.konversi, name="konversi")
+    path("konversi/", views.konversi, name="konversi") 
 ]
