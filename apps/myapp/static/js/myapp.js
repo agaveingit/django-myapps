cd7be26c48575e541e900b41a7d93e72ac06ab97
agevavava <holhokgorgok@gmail.com>
2025-09-03 20:01:02 +0700

    is it works?

--- apps/myapp/static/js/myapp.js
+++ apps/myapp/static/js/myapp.js
@@ -1,4 +1,20 @@
-function konversi() {
-    let loading = document.getElementById("loading");
-    let hasil = document.getElementById("hasil");
-}
+
+document.addEventListener("DOMContentLoaded", function(){
+    const loading1 = document.getElementById("loading1")
+    const loading2 = document.getElementById("loading2")
+    const loading3 = document.getElementById("loading3")
+    const loading4 = document.getElementById("loading4")
+    const konversi = document.getElementById("converter")
+    const submitButton = document.getElementById("submitButton")
+
+    if (konversi) {
+        konversi.addEventListener("submit", function(){
+            loading1.style.display = "block"
+            loading2.style.display = "block"
+            loading3.style.display = "block"
+            loading4.style.display = "block"
+            submitButton.disabled = true
+            submitButton.innerText = "Please Wait ..."
+        })
+    }
+})
