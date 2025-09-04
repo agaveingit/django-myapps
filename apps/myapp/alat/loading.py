71fcfc98b05a6e3880dad4f88785e0bcb4480933
agevavava <holhokgorgok@gmail.com>
2025-09-04 07:01:50 +0700

    gimmick loading

---
+++ apps/myapp/alat/loading.py
@@ -0,0 +1,10 @@
+import time
+
+class Loading:
+    def __init__(self):
+        self.load1 = "Mengambil data dari input ...."
+        self.load2 = "Memproses data ...."
+        self.load3 = "Mengkalkulasikan data"
+        self.load4 = "Memperhitungkan posisi kuantum"
+    def load():
+        return time.sleep(0.8)
--- apps/myapp/urls.py
+++ apps/myapp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("konversi/", views.konversi, name="konversi") 
+    path("konversi/", views.konversi, name="konversi"),
+    path("qr_generator/")
 ]
