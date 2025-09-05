7e7b1cf1532667ce7a828969af92026a98941deb
agevavava <holhokgorgok@gmail.com>
2025-09-05 19:07:38 +0700

    try oop

--- apps/myapp/alat/kodeqr.py
+++ apps/myapp/alat/kodeqr.py
@@ -1,23 +1,46 @@
 import qrcode
 import io
 import base64
+from qrcode.image.svg import SvgImage
 
-def generate_qrcode(data: str):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-        )
-    qr.add_data(data)
-    qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
-    # img.save(filename)
+class GenerateQRCode:
+    def __init__(self):
+        self.file_type_png: str = "png"
+        self.file_type_svg: str = "svg"
 
-    buffer = io.BytesIO()
-    img.save(buffer, format="PNG")
-    buffer.seek(0)
+    def qrcode_img(self, data: str) -> str:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+            )
+        qr.add_data(data)
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        # img.save(filename)
 
-    img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
-    return img_base64
-# bisa dah
+        buffer = io.BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        img_base64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
+        return img_base64
+
+    def qrcode_svg(self, data: str) -> str:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+            )
+        qr.add_data(data)
+        qr.make(fit=True)
+        svg = qr.make_image(image_factory=SvgImage)
+        # img.save(filename)
+
+        buffer = io.BytesIO()
+        svg.save(buffer)
+        svg_data = buffer.getvalue().decode()
+
+        return svg_data
--- apps/myapp/views.py
+++ apps/myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .alat.konversi import Konverter
-from .alat.kodeqr import generate_qrcode
+from .alat.kodeqr import GenerateQRCode
 
 # Create your views here.
 def home(request):
@@ -21,12 +21,19 @@ def konversi(request):
     return render(request, "myapp/konversi.html", {"result": result, "error": error})
 
 def qr_generator(request):
+    file_type: str = None
     data: str = None
     qr_code = None
-    if request.method == "POST":
-        data: str = request.POST.get("data", "kosong")
-        qr_code = generate_qrcode(data)
+    qr = GenerateQRCode()
+    if request.method =="POST":
+        if file_type == "png":
+            data: str = request.POST.get("data", "kosong")
+            qr_code = qr.qrcode_img(data)
+        elif file_type == "svg":
+            data: str = request.POST.get("data", "kosong")
+            qr_code = qr.qrcode_svg(data)
     else:
         data: str = "kosong"
+        return "ERROR! Please try again"
 
     return render(request, "myapp/qr_generator.html", {"qr_code": qr_code})
